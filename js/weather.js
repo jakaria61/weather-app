fc287470716a8ff8c5c8b5f6aac806c62bb8b678
jakaria61 <bdjakaria629@gmail.com>
2021-09-02 23:13:56 +0600

    deg add

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const loadTemp = () => {
     const searchText = inputName.value;
     inputName.value = '';
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=37afafe201c8656df230a7fd84b332ee`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=37afafe201c8656df230a7fd84b332ee&units=metric`
     fetch(url)
         .then(res => res.json())
         .then(data => showWeather(data))
@@ -16,7 +16,7 @@ const showWeather = (data) => {
                         <h1>${data.name}, ${data.sys.country}</h1>
                         <h3>des:${data.weather[0].description}</h3>
                          <h3> main:${data.weather[0].main}</h3>
-                         <h3> deg:${data.wind.deg}</h3>
+                         <h3> deg:${data.main.temp}</h3>
                          <h3>air speed:${data.wind.speed}</h3>
                         <img  src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"/>
                          <h3>TimeZone:${data.timezone}</h3>
